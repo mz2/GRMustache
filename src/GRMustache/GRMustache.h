e999a710b5e1c9c9d62c4e1de717958cacb523cb
mz2 <matias.piipari@gmail.com>
2015-12-30 11:54:08 +0200

    Adds a framework target.

---
+++ src/GRMustache/GRMustache.h
@@ -0,0 +1,19 @@
+//
+//  GRMustache.h
+//  GRMustache
+//
+//  Created by Matias Piipari on 30/12/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GRMustache.
+FOUNDATION_EXPORT double GRMustacheVersionNumber;
+
+//! Project version string for GRMustache.
+FOUNDATION_EXPORT const unsigned char GRMustacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRMustache/PublicHeader.h>
+
+
