d46368da2a7bb06b2f58b8f1dc6bc3781704ed05
mz2 <matias.piipari@gmail.com>
2015-12-30 18:06:51 +0200

    Adds a modulemap

--- src/GRMustache/GRMustache.h
+++ src/GRMustache/GRMustache.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char GRMustacheVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GRMustache/PublicHeader.h>
 
-
+#import "GRMustacheAvailabilityMacros.h"
