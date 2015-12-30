5a41f8deb50458159dac58ea9d64867c92cc5ce1
mz2 <matias.piipari@gmail.com>
2015-12-30 18:09:27 +0200

    Module umbrella header got some meat.

--- src/GRMustache/GRMustache.h
+++ src/GRMustache/GRMustache.h
@@ -16,4 +16,19 @@ FOUNDATION_EXPORT const unsigned char GRMustacheVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GRMustache/PublicHeader.h>
 
+#import "GRMustacheConfiguration.h"
+#import "GRMustacheTemplateRepository.h"
+#import "GRMustacheTemplate.h"
+#import "GRMustacheParser.h"
+#import "GRMustacheTag.h"
+#import "GRMustacheContext.h"
+#import "GRMustacheFilter.h"
+#import "GRMustacheRendering.h"
+#import "GRMustacheLocalizer.h"
+#import "NSFormatter+GRMustache.h"
+#import "NSValueTransformer+GRMustache.h"
+#import "GRMustacheContentType.h"
+#import "GRMustacheError.h"
+#import "GRMustacheVersion.h"
+
 #import "GRMustacheAvailabilityMacros.h"
